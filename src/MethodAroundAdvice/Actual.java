0c4f1e55d72745f25181afc146d00fee30096215
Rishabh Umredkar <umredkarrishabh@gmail.com>
2023-09-14 17:51:04 +0530

    AroundAdvice

---
+++ src/MethodAroundAdvice/Actual.java
@@ -0,0 +1,9 @@
+package MethodAroundAdvice;
+
+public class Actual {
+
+	public void m()
+	{
+		System.out.println("Actual Business Logic");
+	}
+}
---
+++ src/MethodAroundAdvice/AroundAdvisor.java
@@ -0,0 +1,21 @@
+package MethodAroundAdvice;
+import java.lang.reflect.Method;
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+import org.springframework.aop.AfterReturningAdvice;
+
+
+public class AroundAdvisor  implements MethodInterceptor{
+
+	@Override
+	public Object invoke(MethodInvocation mi) throws Throwable {
+		Object obj;
+		System.out.println("additional concern before actual logic");  
+		obj = mi.proceed();
+        System.out.println("additional concern after actual logic");  
+
+		return obj;
+	}
+
+}
