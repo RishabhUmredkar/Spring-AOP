e5a21d9c62fefc0476d347f6deb4b39348e90c24
Rishabh Umredkar <umredkarrishabh@gmail.com>
2023-09-15 17:57:05 +0530

    ThrowsAdvice

--- src/MethodThrowAdvice/throwAdvisor.java
+++ src/MethodThrowAdvice/throwAdvisor.java
@@ -3,7 +3,7 @@ package MethodThrowAdvice;
 import org.springframework.aop.ThrowsAdvice;
 
 public class throwAdvisor  implements ThrowsAdvice{
-	private void afterThrowing(Exception ex) {
+	public void afterThrowing(Exception ex) {
 		System.out.println("additional concern if exception is occur");
 	}
 }
